953396347fa87359787d0a0384b5852ac2d4260f
kikato <35584539+kikato2022@users.noreply.github.com>
2023-01-05 23:37:20 +0800

    Add files via upload

---
+++ ch13/13.1_tabtenn.h
@@ -0,0 +1,21 @@
+// tabtenn.h -- a table-tennis base class
+#ifndef TABTENN_H_
+#define TABTENN_H_
+
+#include<string>
+using std::string;
+
+// simple base class
+class TableTennisPlayer{
+private:
+    string firstname;
+    string lastname;
+    bool hasTable;
+public:
+    TableTennisPlayer(const string & fn = "none", const string & ln = "none", bool ht = false);
+    void Name() const;
+    bool HasTable() const { return hasTable; }
+    void ResetTable(bool v) {hasTable = v;}
+};
+
+#endif
